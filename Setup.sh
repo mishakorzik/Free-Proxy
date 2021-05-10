139d31369f54995463750bca58c9411bf5d44231
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-10 16:53:51 +0300

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -10,8 +10,8 @@ apt-get install git
 apt-get install python2
 pip2 install requests
 pip2 install bs4
-chmod +x Proxy-Search
-cd Proxy-Search
+chmod +x Free-Proxy
+cd Free-Proxy
 sleep 1
 echo succesfull installed!
 sleep 1 
@@ -20,4 +20,4 @@ rm -rf IMG_20210510_150700.jpg
 rm -rf IMG_20210510_150436.jpg
 rm -rf IMG_20210510_145354.jpg
 toilet -F gay Done
-echo Run command: python2 Proxy-Search.py
+echo Run command: python2 Free-Proxy.py
