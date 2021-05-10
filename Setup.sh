4084241fffb31c06dbef6dd3bc350bdb2a3c7b96
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-10 16:51:04 +0300

    Create Setup.sh

---
+++ Setup.sh
@@ -0,0 +1,23 @@
+clear
+sleep 1
+echo Installing packages please wait...
+echo By mishakorzhik
+sleep 2
+cd
+apt-get update
+apt-get upgrade
+apt-get install git 
+apt-get install python2
+pip2 install requests
+pip2 install bs4
+chmod +x Proxy-Search
+cd Proxy-Search
+sleep 1
+echo succesfull installed!
+sleep 1 
+rm -rf IMG_20210510_150717.jpg
+rm -rf IMG_20210510_150700.jpg
+rm -rf IMG_20210510_150436.jpg
+rm -rf IMG_20210510_145354.jpg
+toilet -F gay Done
+echo Run command: python2 Proxy-Search.py
