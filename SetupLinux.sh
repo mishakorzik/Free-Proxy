ca72c412eacad54676284b79d549f04566b331cd
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-10 16:52:57 +0300

    Create SetupLinux.sh

---
+++ SetupLinux.sh
@@ -0,0 +1,23 @@
+clear
+sleep 1
+echo Installing packages please wait...
+echo By mishaKorzhik
+sleep 1
+echo Installing for Linux...
+echo 2
+cd
+apt-get update
+apt-get upgrade
+apt-get install git 
+apt-get install python2
+sleep 1
+echo Please wait be patient.
+sleep 1
+pip2 install requests
+pip2 install bs4
+chmod +x Free-Proxy
+sleep 1
+echo succesfull installed for linux!
+sleep 1 
+toilet -F gay Done
+echo Run command: python2 Free-Proxy.py
