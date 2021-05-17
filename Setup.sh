32d83b95dc773a15327437ea6aee11fa685a05b5
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-17 20:34:52 +0300

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -21,27 +21,12 @@ rm -rf IMG_20210510_150436.jpg
 rm -rf IMG_20210510_145354.jpg
 rand1=$( shuf -i 0-${#colors[@]} -n 1 )
 rand2=$( shuf -i 0-${#colors[@]} -n 1 )
-# Colors
-r='\e[91m'
-g='\e[92m'
-y='\e[93m'
-b='\e[94m'
-p='\e[95m'
-c='\e[96m'
-w='\e[97m'
-n='\e[0m'
-# effect colors
-bd='\e[1m'
-dm='\e[2m'
-it='\e[3m'
-ul='\e[4m'
-rv='\e[7m'
-echo -e "\t${colors[rand1]} ██████╗░░█████╗░███╗░░██╗███████╗
-echo -e "\t${colors[rand1]} ██╔══██╗██╔══██╗████╗░██║██╔════╝
-echo -e "\t${colors[rand1]} ██║░░██║██║░░██║██╔██╗██║█████╗░░
-echo -e "\t${colors[rand1]} ██║░░██║██║░░██║██║╚████║██╔══╝░░
-echo -e "\t${colors[rand1]} ██████╔╝╚█████╔╝██║░╚███║███████╗
-echo -e "\t${colors[rand1]} ╚═════╝░░╚════╝░╚═╝░░╚══╝╚══════╝
-echo -e "\t${colors[rand1]}         Developer: mishakorzhik
-echo -e "\t${colors[rand1]}          Update on: 16 05 2021
-echo -e "\t${colors[rand2]}    Run command: python2 Free-Proxy.py
+echo ██████╗░░█████╗░███╗░░██╗███████╗
+echo ██╔══██╗██╔══██╗████╗░██║██╔════╝
+echo ██║░░██║██║░░██║██╔██╗██║█████╗░░
+echo ██║░░██║██║░░██║██║╚████║██╔══╝░░
+echo ██████╔╝╚█████╔╝██║░╚███║███████╗
+echo ╚═════╝░░╚════╝░╚═╝░░╚══╝╚══════╝
+echo Developer: mishakorzhik
+echo Update on: 17 05 2021
+echo Run command: python2 Free-Proxy.py
