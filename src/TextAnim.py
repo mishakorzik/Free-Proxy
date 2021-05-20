daffda9d9dcf7de11c63f6f58d04075e72b51e29
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-20 20:29:43 +0300

    Update TextAnim.py

--- src/TextAnim.py
+++ src/TextAnim.py
@@ -1,23 +1,7 @@
-import time
-import itertools
-import threading
-import sys
-
-done = False
- 
-def animate():
-    for c in itertools.cycle(['Starting', 'sTarting', 'stArting', 'staRting', 'starTing','startIng', 'startiNg', 'StartinG']):
-        if done:
-            break
-        sys.stdout.write('\r ' + c)
-        sys.stdout.flush()
-        time.sleep(0.1)
-    sys.stdout.write('')
-
-t = threading.Thread(target=animate)
-t.start()
-
- 
-time.sleep(1)
-done = True
-
+print("")
+print("░██████╗████████╗░█████╗░██████╗░████████╗██╗███╗░░██╗░██████╗░")
+print("██╔════╝╚══██╔══╝██╔══██╗██╔══██╗╚══██╔══╝██║████╗░██║██╔════╝░")
+print("╚█████╗░░░░██║░░░███████║██████╔╝░░░██║░░░██║██╔██╗██║██║░░██╗░")
+print("░╚═══██╗░░░██║░░░██╔══██║██╔══██╗░░░██║░░░██║██║╚████║██║░░╚██╗")
+print("██████╔╝░░░██║░░░██║░░██║██║░░██║░░░██║░░░██║██║░╚███║╚██████╔╝")
+print("╚═════╝░░░░╚═╝░░░╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝╚═╝░░╚══╝░╚═════╝░")
