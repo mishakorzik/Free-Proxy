20b92f6e2c032486901d9409396a294f358850c1
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-20 21:04:14 +0300

    Create menu.py

---
+++ src/menu.py
@@ -0,0 +1,11 @@
+print("Enter 1 To  Start proxy")
+print("Enter 2 To  Update Proxy")
+print("Enter 3 To  Exit utility")
+options = input(": ")
+if options == "1":
+	python2 Free-Proxy.py
+if options == "2":
+	print("please copy command & paste command & start command.")
+	print("cd && rm -rf Free-Proxy && pkg install git && git clone https://github.com/mishakorzik/Free-Proxy && cd Free-Proxy && echo Succesfull installed!")
+if options =="3":
+	exit
