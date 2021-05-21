26d951901ec74571de29edde1dd7f9e7e8d70ea0
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-21 23:17:39 +0300

    Create OtherInstall.sh

---
+++ packages/OtherInstall.sh
@@ -0,0 +1,35 @@
+apt-get update
+apt-get upgrade
+apt-get upgraded
+sleep 1
+echo -e $b">"$w" installing modules: "$g"git"$w
+sleep 1
+apt-get install git
+sleep 1
+echo -e $b">"$w" installing modules: "$g"python2"$w
+sleep 1
+apt-get install python2
+sleep 1
+echo -e $b">"$w" installing modules: "$g"requests"$w
+sleep 1
+pip2 install requests
+sleep 1
+echo -e $b">"$w" installing modules: "$g"bs4"$w
+sleep 1
+pip2 install bs4
+cd
+cd Free-Proxy 
+rm -rf IMG_20210510_150717.jpg
+rm -rf IMG_20210510_150700.jpg
+rm -rf IMG_20210510_150436.jpg
+rm -rf IMG_20210510_145354.jpg
+echo ██████╗░░█████╗░███╗░░██╗███████╗
+echo ██╔══██╗██╔══██╗████╗░██║██╔════╝
+echo ██║░░██║██║░░██║██╔██╗██║█████╗░░
+echo ██║░░██║██║░░██║██║╚████║██╔══╝░░
+echo ██████╔╝╚█████╔╝██║░╚███║███████╗
+echo ╚═════╝░░╚════╝░╚═╝░░╚══╝╚══════╝
+echo Developer: mishakorzhik
+echo Created  : 21 05 2021
+echo Run command: bash FreeProxy.sh
+
