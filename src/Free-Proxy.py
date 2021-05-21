d06b9ad17dd440a8b5816719dd510e45205d14ac
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-21 14:08:53 +0300

    Update Free-Proxy.py

--- src/Free-Proxy.py
+++ src/Free-Proxy.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 # -*- coding: utf-8 
 
 Green="\033[1;33m"
@@ -8,17 +8,7 @@ Reset="\033[0m"
 Red="\033[1;31m"
 Purple="\033[0;35m"
 
-print " "+Green+"██████╗░██████╗░░█████╗░██╗░░██╗██╗░░░██╗"
-print " "+Green+"██╔══██╗██╔══██╗██╔══██╗╚██╗██╔╝╚██╗░██╔╝"
-print " "+Green+"██████╔╝██████╔╝██║░░██║░╚███╔╝░░╚████╔╝░"
-print " "+Green+"██╔═══╝░██╔══██╗██║░░██║░██╔██╗░░░╚██╔╝░░"
-print " "+Green+"██║░░░░░██║░░██║╚█████╔╝██╔╝╚██╗░░░██║░░░"
-print " "+Green+"╚═╝░░░░░╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝░░░╚═╝░░░ v1.3"
-print ""
-print " "+Green+"          Developer : mishakorzhik"
-print " "+Green+"          Code      : bash, python"
-print ""
-print " "+Blue+""
+os.system("cd && cd Free-Proxy && cd src && python2 Logo.py && cd && cd Free-Proxy")
 
 import time
 import itertools
