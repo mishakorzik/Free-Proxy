5a60cc6fde8a0161a9312783a5bac0804e90e706
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-21 21:50:25 +0300

    Update menu.py

--- src/menu.py
+++ src/menu.py
@@ -12,69 +12,7 @@ Reset="\033[0m"
 Red="\033[1;31m"
 Purple="\033[0;35m"
 
-
-color1=["\033[1;34m","\033[1;34m","\033[1;34m","\033[1;34m","\033[1;34m","\033[1;34m"]
-def color():
- return str(random.choice(color1))
-def generate(url,count):
- try:
-  r=requests.get("https://www.socks-proxy.net")
-  soup=BeautifulSoup(r.content,'html.parser')
-  list = soup.find(class_='table table-striped table-bordered')
-  list1 = list.findAll('td')
-  iplist=[]
-  portlist=[]
-  codelist=[]
-  countrylist=[]
-  versionlist=[]
-  anonymitylist=[]
-  httpslist=[]
-  last_checklist=[]
-  j=0
-  while(j<8*count):
-   a=str((list1[j].contents[0]))
-   iplist.append(a)
-   j=j+8
-  j=1
-  while(j<8*count):
-   a=str((list1[j].contents[0]))
-   portlist.append(a)
-   j=j+8
-  j=2
-  while(j<8*count):
-   a=str((list1[j].contents[0]))
-   codelist.append(a)
-   j=j+8
-  j=3
-  while(j<8*count):
-   a=str((list1[j].contents[0]))
-   countrylist.append(a)
-   j=j+8
-  j=4
-  while(j<8*count):
-   a=str((list1[j].contents[0]))
-   versionlist.append(a)
-   j=j+8
-  j=5
-  while(j<8*count):
-   a=str((list1[j].contents[0]))
-   anonymitylist.append(a)
-   j=j+8
-  j=6
-  while(j<8*count):
-   a=str((list1[j].contents[0]))
-   httpslist.append(a)
-   j=j+8
-  j=7
-  while(j<8*count):
-   a=str((list1[j].contents[0]))
-   last_checklist.append(a)
-   j=j+8
-
-  for k in range (0,count):
-   print(color()+iplist[k]+":"+color()+portlist[k]+"\t"+color()+codelist[k]+"\t"+color()+countrylist[k]+"\t"+color()+versionlist[k]+"\t"+color()+anonymitylist[k]+"\thttps:"+color()+httpslist[k]+"\tLast Checked :"+color()+last_checklist[k]+"\n")
- except:
-color1=["\033[1;34m","\033[1;34m","\033[1;34m","\033[1;34m","\033[1;34m","\033[1;34m"]
+color1=["\033[1;31;40m","\033[1;32;40m","\033[1;33;40m","\033[1;34;40m","\033[1;35;40m","\033[1;36;40m"]
 def color():
  return str(random.choice(color1))
 def generate(url,count):
@@ -132,7 +70,67 @@ def generate(url,count):
    a=str(list1[j].contents[0])
    last_checklist.append(a)
    j=j+8
-    for k in range (0,count):
+  for k in range (0,count):
+      print(color()+iplist[k]+":"+color()+portlist[k]+"\t"+color()+codelist[k]+"\t"+color()+countrylist[k]+"\t\t"+color()+anonymitylist[k]+color()+"\tGoogle:"+googlelist[k]+color()+"\thttps:"+httpslist[k]+color()+"\tLast checked "+last_checklist[k]+"\n")
+ except:
+  print("\033[1;31;40m%d Proxies can't be generated at this time. Try giving a smaller amount(i.e 1) or try again after some time."%count)
+def socks(count):
+ try:
+  r=requests.get("https://www.socks-proxy.net")
+  soup=BeautifulSoup(r.content,'html.parser')
+  list = soup.find(class_='table table-striped table-bordered')
+  list1 = list.findAll('td')
+  iplist=[]
+  portlist=[]
+  codelist=[]
+  countrylist=[]
+  versionlist=[]
+  anonymitylist=[]
+  httpslist=[]
+  last_checklist=[]
+  j=0
+  while(j<8*count):
+   a=str((list1[j].contents[0]))
+   iplist.append(a)
+   j=j+8
+  j=1
+  while(j<8*count):
+   a=str((list1[j].contents[0]))
+   portlist.append(a)
+   j=j+8
+  j=2
+  while(j<8*count):
+   a=str((list1[j].contents[0]))
+   codelist.append(a)
+   j=j+8
+  j=3
+  while(j<8*count):
+   a=str((list1[j].contents[0]))
+   countrylist.append(a)
+   j=j+8
+  j=4
+  while(j<8*count):
+   a=str((list1[j].contents[0]))
+   versionlist.append(a)
+   j=j+8
+  j=5
+  while(j<8*count):
+   a=str((list1[j].contents[0]))
+   anonymitylist.append(a)
+   j=j+8
+  j=6
+  while(j<8*count):
+   a=str((list1[j].contents[0]))
+   httpslist.append(a)
+   j=j+8
+  j=7
+  while(j<8*count):
+   a=str((list1[j].contents[0]))
+   last_checklist.append(a)
+   j=j+8
+
+  for k in range (0,count):
+   print(color()+iplist[k]+":"+color()+portlist[k]+"\t"+color()+codelist[k]+"\t"+color()+countrylist[k]+"\t"+color()+versionlist[k]+"\t"+color()+anonymitylist[k]+"\thttps:"+color()+httpslist[k]+"\tLast Checked :"+color()+last_checklist[k]+"\n")
  except:
   print("\033[1;31;40m%d Proxies can't be generated at this time. Try giving a smaller amount(i.e 1) or try again after some time."%count)
 
