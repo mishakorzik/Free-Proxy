c63e66277dc113ed47bcc7cadb3d675b3180bce6
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-21 13:51:53 +0300

    Update menuPython2.py

--- src/menuPython2.py
+++ src/menuPython2.py
@@ -5,6 +5,72 @@ import time
 import random
 import sys
 
+os.system("apt install toilet")
+color1=["\033[1;31;40m","\033[1;32;40m","\033[1;33;40m","\033[1;34;40m","\033[1;35;40m","\033[1;36;40m"]
+def color():
+ return str(random.choice(color1))
+def generate(url,count):
+ try:
+  r=requests.get(url)
+  soup=BeautifulSoup(r.content,'html.parser')
+  list = soup.find(class_='table table-striped table-bordered')
+  list1 = list.findAll('td')
+  iplist=[]
+  portlist=[]
+  codelist=[]
+  countrylist=[]
+  anonymitylist=[]
+  googlelist=[]
+  httpslist=[]
+  last_checklist=[]
+  finallist=[]
+  j=0
+  while(j<(8*count)):
+   a=str(list1[j].contents[0])
+   iplist.append(a)
+   j=j+8
+  j=1
+  while(j<(8*count)):
+   a=str(list1[j].contents[0])
+   portlist.append(a)
+   j=j+8
+  j=2   
+  while(j<(8*count)):
+   a=str(list1[j].contents[0])
+   codelist.append(a)
+   j=j+8   
+  j=3   
+  while(j<(8*count)):
+   a=str(list1[j].contents[0])
+   countrylist.append(a)
+   j=j+8 
+  j=4   
+  while(j<(8*count)):
+   a=str(list1[j].contents[0])
+   anonymitylist.append(a)
+   j=j+8
+  j=5   
+  while(j<(8*count)):
+   a=str(list1[j].contents[0])
+   googlelist.append(a)
+   j=j+8  
+  j=6   
+  while(j<(8*count)):
+   a=str(list1[j].contents[0])
+   httpslist.append(a)
+   j=j+8  
+  j=7   
+  while(j<(8*count)):
+   a=str(list1[j].contents[0])
+   last_checklist.append(a)
+   j=j+8
+  for k in range (0,count):
+      print(color()+iplist[k]+":"+color()+portlist[k]+"\t"+color()+codelist[k]+"\t"+color()+countrylist[k]+"\t\t"+color()+anonymitylist[k]+color()+"\tGoogle:"+googlelist[k]+color()+"\thttps:"+httpslist[k]+color()+"\tLast checked "+last_checklist[k]+"\n")
+ except:
+  print("\033[1;31;40m%d Proxies can't be generated at this time. Try giving a smaller amount(i.e 1) or try again after some time."%count)
+def socks(count):
+ try:
+
 Green="\033[1;33m"
 Blue="\033[1;34m"
 Grey="\033[1;30m"
