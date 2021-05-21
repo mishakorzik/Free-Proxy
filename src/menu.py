5c3341a802102fe272c00fa5a37f553b683c9f36
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-21 21:28:24 +0300

    Update menu.py

--- src/menu.py
+++ src/menu.py
@@ -12,6 +12,64 @@ Reset="\033[0m"
 Red="\033[1;31m"
 Purple="\033[0;35m"
 
+def socks(count):
+ try:
+  r=requests.get("https://www.socks-proxy.net")
+  soup=BeautifulSoup(r.content,'html.parser')
+  list = soup.find(class_='table table-striped table-bordered')
+  list1 = list.findAll('td')
+  iplist=[]
+  portlist=[]
+  codelist=[]
+  countrylist=[]
+  versionlist=[]
+  anonymitylist=[]
+  httpslist=[]
+  last_checklist=[]
+  j=0
+  while(j<8*count):
+   a=str((list1[j].contents[0]))
+   iplist.append(a)
+   j=j+8
+  j=1
+  while(j<8*count):
+   a=str((list1[j].contents[0]))
+   portlist.append(a)
+   j=j+8
+  j=2
+  while(j<8*count):
+   a=str((list1[j].contents[0]))
+   codelist.append(a)
+   j=j+8
+  j=3
+  while(j<8*count):
+   a=str((list1[j].contents[0]))
+   countrylist.append(a)
+   j=j+8
+  j=4
+  while(j<8*count):
+   a=str((list1[j].contents[0]))
+   versionlist.append(a)
+   j=j+8
+  j=5
+  while(j<8*count):
+   a=str((list1[j].contents[0]))
+   anonymitylist.append(a)
+   j=j+8
+  j=6
+  while(j<8*count):
+   a=str((list1[j].contents[0]))
+   httpslist.append(a)
+   j=j+8
+  j=7
+  while(j<8*count):
+   a=str((list1[j].contents[0]))
+   last_checklist.append(a)
+   j=j+8
+
+  for k in range (0,count):
+   print(color()+iplist[k]+":"+color()+portlist[k]+"\t"+color()+codelist[k]+"\t"+color()+countrylist[k]+"\t"+color()+versionlist[k]+"\t"+color()+anonymitylist[k]+"\thttps:"+color()+httpslist[k]+"\tLast Checked :"+color()+last_checklist[k]+"\n")
+ except:
 color1=["\033[1;34m","\033[1;34m","\033[1;34m","\033[1;34m","\033[1;34m","\033[1;34m"]
 def color():
  return str(random.choice(color1))
@@ -108,6 +166,7 @@ elif(op==4):
 elif(op==5):
  generate("https://free-proxy-list.net/anonymous-proxy.html",80)
 elif(op==6):
+ socks(120)
  print " "+Blue+"177.206.186.9:8080   SOCKS4   Brazil   Elite"
  print " "+Blue+"51.81.31.62:54860   SOCKS5  Francia    hing anonymous"
 elif(op==7):
