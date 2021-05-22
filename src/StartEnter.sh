37195bb6fc0dcb3b8ccc3985dfdb5dd0cd905f60
DeplayGo <68460974+mishakorzik@users.noreply.github.com>
2021-05-22 11:56:55 +0300

    Update StartEnter.sh

--- src/StartEnter.sh
+++ src/StartEnter.sh
@@ -13,10 +13,6 @@ echo "Please press enter to launch..."
 read a1
 echo -e $b">"$w" installing modules: "$g"python2"$w
 pkg install python2
-echo -e $b">"$w" installing modules: "$g"pip3"$w
-pkg install pip3
-echo -e $b">"$w" installing modules: "$g"requests"$w
-pip3 install requests
 clear
 clear
 sleep 1
