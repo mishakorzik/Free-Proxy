bc662a2ff1801325df37cf18e9b689746b369c00
He1Zen <68460974+mishakorzik@users.noreply.github.com>
2022-01-28 13:06:10 +0200

    Update Free-Proxy.sh

--- src/Free-Proxy.sh
+++ src/Free-Proxy.sh
@@ -7,8 +7,8 @@ echo '
 ██║░░░░░██║░░██║███████╗███████╗░░░░░░██║░░░░░██║░░██║╚█████╔╝██╔╝╚██╗░░░██║░░░
 ╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚══════╝░░░░░░╚═╝░░░░░╚═╝░░╚═╝░╚════╝░╚═╝░░╚═╝░░░╚═╝░░░ v1.6
 
-             # Developer : mishakorzhik (DeplayGo)
-             # version : Free-Proxy version 1.5
+             # Developer : mishakorzhik (He1Zen)
+             # version : Free-Proxy version 1.6
              # Code  : bash, python
 '|lolcat -p 1.7
 
